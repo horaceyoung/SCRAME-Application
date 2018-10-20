8e19bbefa2e27cc6f1c8a53da53881f2452e6b36
HORACEYOUNG <32321396+HORACEYOUNG@users.noreply.github.com>
2018-10-20 16:12:28 +0800

    data update

---
+++ src/Exceptions/TutorialLabNumberInvalidException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class TutorialLabNumberInvalidException ex{
+    public TutorialLabNumberInvalidException(){
+        super("TutorialLabNumberInvalidException: the to");
+    }
+}
