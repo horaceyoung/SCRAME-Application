f5d98768add315950cf8594596aa38cd4c190d93
yinya998 <42985754+yinya998@users.noreply.github.com>
2018-10-21 09:22:51 +0800

    Update StudentManager.java

--- src/com/group1/StudentManager.java
+++ src/com/group1/StudentManager.java
@@ -1,4 +1,29 @@
 package com.group1;
 
+import Exceptions.NameNotValidException;
+
+import java.util.Scanner;
+
 public class StudentManager {
+    public static Student AddStudent (){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Add Student: Please input the Student Name.");        
+        String studentName = "Default";
+        boolean nameValid = false;
+        while(!nameValid) {
+            studentName = sc.nextLine();
+            try {
+                if (!InputManager. ValidateNameInput(studentName)) 
+                      throw new NameNotValidException ();
+                
+                nameValid = true;
+                System.out.println("Add student Success: student " + studentName + " has been successfully added");
+
+            } catch (NameNotValidException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return new Student(studentName);
+    }
+
 }
