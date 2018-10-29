0920743bb98e04f6072c3af7cb0dec068a6ebeea
cyberwander <cyberwanderwayne@gmail.com>
2018-10-29 20:16:52 +0800

    debug 10

--- src/com/group1/StudentManager.java
+++ src/com/group1/StudentManager.java
@@ -5,6 +5,7 @@ import Exceptions.NameNotValidException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.FileReader;
 import java.io.BufferedReader;
@@ -40,7 +41,8 @@ public class StudentManager {
     }
     
     public static void printTranscript(String studentMatricTranscript) throws FileNotFoundException{
-        File resultsFile = new File("data/Results.txt");
+    	
+    	File resultsFile = new File("data/Results.txt");
         Scanner resultsScanner = new Scanner(resultsFile);
         String studentName;
        
@@ -53,12 +55,27 @@ public class StudentManager {
             if (matricNumber[0].toUpperCase().equals(studentMatricTranscript.toUpperCase())) {
             	studentName = Student.getNameFromMatric(studentMatricTranscript);
             	
+            	//get weight of exam and coursework
+            	File openComponentFile = new File("data/Component.txt");
+                Scanner openComponentScanner = new Scanner (openComponentFile);
+                float examWeightage=0;
+                float courseworkWeightage=0;
+                while(openComponentScanner.hasNext()) {
+            		String[] courseName = openComponentScanner.nextLine().split("\t");
+            		if (courseName[0].toUpperCase().equals(matricNumber[1].toUpperCase())) {
+            			String[] tempExam = courseName[1].split(": ");
+            			String[] tempcoursework = courseName[2].split(": ");
+            			examWeightage = Float.parseFloat(tempExam[1]);
+            			courseworkWeightage = Float.parseFloat(tempcoursework[1]);
+            			}
+            		}
+            	
             	//get overall score for the course
         		String number2 = matricNumber[2];
 				float result2 = Float.parseFloat(number2.trim());
     			String number3 = matricNumber[3];
   				float result3 = Float.parseFloat(number3.trim());	
-  				float result = result2 + result3;
+  				float result = result2 * examWeightage + result3 * courseworkWeightage;
 				
             	System.out.println(studentName);
             	for (int i = 4; i< matricNumber.length; i++) {
@@ -86,11 +103,28 @@ public class StudentManager {
     }
     
     public static void printCourseStatistics(String courseCodeStatistics) throws FileNotFoundException{
-        File resultsCourseFile = new File("data/Results.txt");
+        
+    	//get the weight of exam and coursework
+    	File componentFile = new File("data/Component.txt");
+        Scanner ComponentScanner = new Scanner (componentFile);
+        float examWeightage=0;
+        float courseworkWeightage=0;
+        while(ComponentScanner.hasNext()) {
+    		String[] courseName = ComponentScanner.nextLine().split("\t");
+    		if (courseName[0].toUpperCase().equals(courseCodeStatistics.toUpperCase())) {
+    			String[] tempExam = courseName[1].split(": ");
+    			String[] tempcoursework = courseName[2].split(": ");
+    			examWeightage = Float.parseFloat(tempExam[1]);
+    			courseworkWeightage = Float.parseFloat(tempcoursework[1]);
+    			}
+    		}
+        
+    	File resultsCourseFile = new File("data/Results.txt");
         Scanner resultsScanner = new Scanner(resultsCourseFile);
         float examResult = 0;
-        int studentCount=0;
         float courseWorkResult = 0;
+        int studentCount=0;
+        
         //read Results file to get results
         while (resultsScanner.hasNext()) {
             String[] courseCode = resultsScanner.nextLine().split("\t");
@@ -105,6 +139,6 @@ public class StudentManager {
         System.out.println(
     			"\n\tExam Result: " + examResult/studentCount + "%" + 
     			"\n\tcoursework Result: " + courseWorkResult/studentCount + "%" +
-    			"\n\tOverall(exam + coursework): " + (examResult + courseWorkResult)/studentCount + "%");
+    			"\n\tOverall(exam + coursework): " + (examResult * examWeightage + courseWorkResult * courseworkWeightage)/studentCount + "%");
     }    
 }
