78fdf64b58474486d980edad8f3116e84b919952
splashingsplashes <splashingsplashes@gmail.com>
2018-11-04 11:07:40 +0800

    Update src/com/group1/DataContainer.java

--- src/com/group1/DataContainer.java
+++ src/com/group1/DataContainer.java
@@ -10,7 +10,7 @@ public class DataContainer implements java.io.Serializable{
     public ArrayList<Course> courseList = new ArrayList<>();
     public boolean studentExists(String matricNumber){
         for(Student student:studentsList){
-            if(matricNumber.equals(student.getMatricNumber())
+            if(matricNumber.equals(student.getMatricNumber()))
                 return true;
         }
         return false;
