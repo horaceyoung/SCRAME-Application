6fe6e50802f6ffe04ef6abcf66be234f01e9f91f
splashingsplashes <splashingsplashes@gmail.com>
2018-11-04 17:08:12 +0800

    case 4

--- src/com/group1/ReadingManager.java
+++ src/com/group1/ReadingManager.java
@@ -12,6 +12,7 @@ import java.util.Scanner;
 public class ReadingManager
 {
 
+/*
     public ArrayList<Tutorial> GetCourseTutorials(String courseTitle, DataContainer dataContainer){
         ArrayList<Tutorial> tutorialList = new ArrayList<>();
         for(Course course:dataContainer.getCourseList()){
@@ -31,73 +32,72 @@ public class ReadingManager
         }
         return labList;
     }
-
-
-
-    public static String getNameFromMatric (String marticNumber,DataContainer dataContainer) {
-        try{
-            for(Student currStudent:dataContainer.getStudentsList()) {
-                if(currStudent.getMatricNumber().equals(marticNumber))
-                    return currStudent.GetStudentName();
-            }
-        }
-        catch (Exception e){
-            System.out.println(e.getMessage());
-        }
-        return "Default";
-    }
+*/
 
 
     public static void printStudentListByTutorial(String courseName, String tutGroupName, DataContainer dataContainer) throws IOException {
-        for(Course currCourse:dataContainer.getCourseList())
-        {
-            if(currCourse.GetCourseTitle().equals(courseName.toUpperCase()))
-            {
-                for (Tutorial currTut: currCourse.GetTutorialList()){
-                    System.out.println("Students registered in " + tutGroupName +": ")
+        for(Course currCourse:dataContainer.getCourseList()) {
+            if (currCourse.GetCourseTitle().equals(courseName.toUpperCase())) {
+                for (Tutorial currTut : currCourse.GetTutorialList()) {
+                    System.out.println("Students registered in " + tutGroupName + ": ");
                     {
-                        for ( currTut: currCourse.GetTutorialList()){
+                        for (currTut:
+                             currCourse.GetTutorialList()) {
+                        }
                     }
+                    return;
                 }
-                return;
             }
         }
-
+    }
 
     public static void PrintTutorialVacancy(String courseTitle, DataContainer dataContainer) throws IOException, TutorialLabNotAvailableException {
-        boolean available = false;
-        int occupied;
-        System.out.println("The Vacancy of Tutorial Groups of " + courseTitle + " is as following: ");
-        while(coursesScanner.hasNext()){
-            String[] currentCourse = coursesScanner.nextLine().split("\t");
-            if (currentCourse[0].equals(courseTitle)){
-                available=true;
-                occupied = Integer.parseInt(currentCourse[2])-Integer.parseInt(currentCourse[3]);
-                System.out.print("Tutorial Group "+currentCourse[1]+"\t");
-                System.out.println(occupied+ "/" + currentCourse[2] + "\n");
+        ArrayList<Tutorial> tutorialList = new ArrayList<>();
+        ArrayList<Course> courseList = dataContainer.getCourseList();
+        try {
+            for (Course course : courseList) {
+                if (courseTitle.equals(course.GetCourseTitle())) {
+                    if (course.HaveTutorial() == false)
+                        throw new TutorialLabNotAvailableException();
+                    else tutorialList = course.GetTutorialList();
+
+                }
+            }
+
+            System.out.println("The Vacancy of Tutorial Groups of " + courseTitle + " is as following: ");
+
+            for (Tutorial tutorial : tutorialList) {
+                System.out.println(tutorial.sessionName + " " + tutorial.GetRegisteredStudent().size() + "/" + tutorial.totalVacancy);
+
 
             }
         }
-        if(!available)
-            throw new TutorialLabNotAvailableException();
+        catch (TutorialLabNotAvailableException e){System.out.println(e.getMessage());}
     }
-    public static void PrintLabVacancy(String courseTitle, DataContainer dataContainer) throws IOException, TutorialLabNotAvailableException{
-        File courseFile = new File("data/Labs.txt");
-        Scanner coursesScanner = new Scanner(courseFile);
-        boolean available = false;
-        int occupied;
-        System.out.println("The Vacancy of Lab Groups of " + courseTitle + " is as following: ");
-        while(coursesScanner.hasNext()){
-            String[] currentCourse = coursesScanner.nextLine().split("\t");
-            if (currentCourse[0].equals(courseTitle)){
-                available=true;
-                occupied = Integer.parseInt(currentCourse[2])-Integer.parseInt(currentCourse[3]);
-                System.out.print("Lab Group "+currentCourse[1]+"\t");
-                System.out.println(occupied+ "/" + currentCourse[2] + "\n");
+
+
+
+    public static void PrintLabVacancy(String courseTitle, DataContainer dataContainer) throws IOException, TutorialLabNotAvailableException {
+        ArrayList<Lab> labList = new ArrayList<>();
+        ArrayList<Course> courseList = dataContainer.getCourseList();
+        try {
+            for (Course course : courseList) {
+                if (courseTitle.equals(course.GetCourseTitle())) {
+                    if (course.HaveLab() == false)
+                        throw new TutorialLabNotAvailableException();
+                    else labList= course.GetLabList();
+
+                }
+            }
+
+            System.out.println("The Vacancy of Lab Groups of " + courseTitle + " is as following: ");
+
+            for (Lab lab : labList) {
+                System.out.println(lab.sessionName + " " + lab.GetRegisteredStudent().size() + "/" + lab.totalVacancy);
+
 
             }
         }
-        if(!available)
-            throw new TutorialLabNotAvailableException();
+        catch (TutorialLabNotAvailableException e){System.out.println(e.getMessage());}
     }
 }
