d9e44cd4d6bc5f907131559d0b8ab54187f48710
Yinya <Yinya998@gmail.com>
2018-11-04 14:33:38 +0800

    hi

--- src/com/group1/ReadingManager.java
+++ src/com/group1/ReadingManager.java
@@ -1,7 +1,23 @@
 package com.group1;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class ReadingManager
 {
 
+    public static String getNameFromMatric (String marticNumber,DataContainer dataContainer) {
+        try{
+            for(Student currStudent:dataContainer.getStudentsList()) {
+                if(currStudent.getMatricNumber().equals(marticNumber))
+                    return currStudent.GetStudentName();
+                }
+            }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return "Default";
+    }
     // reading methods all goes in to this (printing.....), combine unnecessary method
 }
+
