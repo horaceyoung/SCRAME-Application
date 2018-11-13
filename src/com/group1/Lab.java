c7934d575daa1ed99dc90ab749f5620a340c33c7
HORACEYOUNG <32321396+HORACEYOUNG@users.noreply.github.com>
2018-11-13 22:48:43 +0800

    balalala

--- src/com/group1/Lab.java
+++ src/com/group1/Lab.java
@@ -1,6 +1,7 @@
 package com.group1;
 
 public class Lab extends Session implements java.io.Serializable{
+    private String facilityRequirements;
     public Lab(String sessionName, int totalVacancy){
         super(sessionName, totalVacancy);
     }
