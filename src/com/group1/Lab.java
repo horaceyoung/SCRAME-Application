d366cab64928db5c6269e4c289637561de2126b5
HORACEYOUNG <32321396+HORACEYOUNG@users.noreply.github.com>
2018-11-13 22:58:14 +0800

    a bit of this a bit of that

--- src/com/group1/Lab.java
+++ src/com/group1/Lab.java
@@ -1,7 +1,6 @@
 package com.group1;
 
 public class Lab extends Session implements java.io.Serializable{
-    private String facilityRequirements;
     public Lab(String sessionName, int totalVacancy){
         super(sessionName, totalVacancy);
     }
--- src/com/group1/Student.java
+++ src/com/group1/Student.java
@@ -29,4 +29,3 @@ public class Student implements java.io.Serializable, People
 	}
 }
 	
-
